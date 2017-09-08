d75efe34a5ef31bbeeec0c7e0dac31b4db2ccc23
goodlygit <31743267+goodlygit@users.noreply.github.com>
2017-09-09 01:22:28 +0530

    Variables

---
+++ Java Project/JavaPractice/src/com/Variables/Variabletest3.java
@@ -0,0 +1,12 @@
+package com.Variables;
+
+public class Variabletest3 
+	{
+		static int a=10;
+	
+public static void main(String[] args)
+	{
+		int a=5;
+		System.out.println("a value is: "+a);
+	}
+}
